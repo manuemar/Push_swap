bd2f87a889298ec1be551356d92c555d97b53cc7
manuemar <122746081+manuemar@users.noreply.github.com>
2024-01-29 19:47:22 +0100

    Add files via upload
    Only the files created, nothing done yet

---
+++ push_swap/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manuemar <manuemar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 18:34:27 by manuemar          #+#    #+#             */
+/*   Updated: 2024/01/29 18:51:34 by manuemar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct s_stack
+{
+	//si
+}	t_stack;
+
+
+#endif
